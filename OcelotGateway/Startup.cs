9bd9aa593a3c493788c90056bd038118f3e97226
DanielMigchels <danielm12@hotmail.nl>
2020-04-14 16:52:15 +0200

    Add Test Gateway Setting

--- OcelotGateway/Startup.cs
+++ OcelotGateway/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Ocelot.Administration;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
@@ -19,7 +20,8 @@ namespace OcelotGateway
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddOcelot();
+            services.AddOcelot().AddAdministration("/administration", "test");
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
