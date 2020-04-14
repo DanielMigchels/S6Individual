2daedadba562c289f29c36ffbf2bf7ec27765bad
DanielMigchels <danielm12@hotmail.nl>
2020-04-14 18:13:47 +0200

    Added ArticleSyndicationService

---
+++ ArticleSyndicationService/Controllers/TestController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ArticleSyndicationService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet("/")]
+        public ActionResult Get()
+        {
+            string URL = "https://www.omroepbrabant.nl/rss";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(URL);
+            HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+            using (Stream responseStream = response.GetResponseStream())
+            {
+                StreamReader reader = new StreamReader(responseStream, Encoding.UTF8);
+                return Ok(reader.ReadToEnd());
+            }
+
+        }
+    }
+}
